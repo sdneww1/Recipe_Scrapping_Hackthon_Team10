2da88a02c6dd903c913841df3b03f43655850b14
KomalKotkar <komalborse31@gmail.com>
2024-07-18 15:13:32 -0500

    Revert "Final code merge from Komal_branch_Recipe to master branch"

--- src/test/java/reciepescrapping_team10_utility/ExcelWriter.java
+++ src/test/java/reciepescrapping_team10_utility/ExcelWriter.java
@@ -20,7 +20,7 @@ public class ExcelWriter {
     public XSSFCell cell;
 
     public void setCellData(String sheetName, int rownum, int column, String data) throws IOException {
-        String path ="C:\\Users\\sagar\\RecipeDB\\Recipe_Scrapping_Hackthon_Team10\\TestData\\outPutData.xlsx";
+        String path ="C:\\Users\\Home\\eclipse-workspace\\Recipe_Scrapping_Hackthon_Team10\\TestData\\outPutData.xlsx";
         File xlfile = new File(path);
         if (!xlfile.exists()) {
             workbook = new XSSFWorkbook();
@@ -47,4 +47,31 @@ public class ExcelWriter {
         fo.close();
     }
     
+    public void setCellAllergyData(String sheetName, int rownum, int column, String data) throws IOException {
+        String path ="C:\\\\Users\\valli\\Git\\Recipe_Scraping_Hackathon_April2023_Data_Scrapers_T15\\src\\test\\resources\\AllergyData.xlsx";
+        File xlfile = new File(path);
+        if (!xlfile.exists()) {
+            workbook = new XSSFWorkbook();
+            fo = new FileOutputStream(path);
+            workbook.write(fo);
+        }
+        fi = new FileInputStream(path);
+        workbook = new XSSFWorkbook(fi);
+
+        if (workbook.getSheetIndex(sheetName) == -1)
+            workbook.createSheet(sheetName);
+        sheet = workbook.getSheet(sheetName);
+
+        if (sheet.getRow(rownum) == null)
+            sheet.createRow(rownum);
+        row = sheet.getRow(rownum);
+
+        cell = row.createCell(column);
+        cell.setCellValue(data);
+        fo = new FileOutputStream(path);
+        workbook.write(fo);
+        workbook.close();
+        fi.close();
+        fo.close();
+    }
 }
